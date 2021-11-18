0e8a5a939bc85c87e153a18745aa5ce68c49f075
sam-dev75 <saitsamir@hotmail.fr>
2021-11-18 19:50:23 +0100

    first

---
+++ project_JS/script_3.js
@@ -0,0 +1,9 @@
+let string = "";
+let n = Number
+
+for (let i = 0; i < n; i--){
+    string += '#';
+    console.log(string);
+}
+
+console.log('Hi, welcome to my super pyramid! How many floors do you want?')
---
+++ project_JS/script_4.js
@@ -0,0 +1,40 @@
+const entrepreneurs = [
+  { first: 'Steve', last: 'Jobs', year: 1955 },
+  { first: 'Oprah', last: 'Winfrey', year: 1954 },
+  { first: 'Bill', last: 'Gates', year: 1955 },
+  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+  { first: 'Beyonce', last: 'Knowles', year: 1981 },
+  { first: 'Jeff', last: 'Bezos', year: 1964 },
+  { first: 'Diane', last: 'Hendricks', year: 1947 },
+  { first: 'Elon', last: 'Musk', year: 1971 },
+  { first: 'Marissa', last: 'Mayer', year: 1975 },
+  { first: 'Walt', last: 'Disney', year: 1901 },
+  { first: 'Larry', last: 'Page', year: 1973 },
+  { first: 'Jack', last: 'Dorsey', year: 1976 },
+  { first: 'Evan', last: 'Spiegel', year: 1990 },
+  { first: 'Brian', last: 'Chesky', year: 1981 },
+  { first: 'Travis', last: 'Kalanick', year: 1976 },
+  { first: 'Marc', last: 'Andreessen', year: 1971 },
+  { first: 'Peter', last: 'Thiel', year: 1967 }
+];
+//in this list the entrepreneurs who were born in the 70s
+
+var hippieYears = entrepreneurs.filter(function(first,last) {
+  if (year >=1970 & year<1980)
+  return hippieYears
+});
+
+// array that contains the first and last names of the entrepreneurs
+
+var listOfEntrepreneurs = entrepreneurs.filter(function(first,last) {
+  
+  return listOfEntrepreneurs
+});
+
+//How old would each inventor be today?
+
+var oldYears = entrepreneurs.filter(function() {
+  
+  return oldYears 
+});
